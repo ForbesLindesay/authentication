2d5d70385c30b7229e71f4aafdde480a733a9938
Forbes Lindesay <forbes@lindesay.co.uk>
2020-07-18 17:27:57 +0100

    test: add some extra test cases

--- packages/cloudflare-ip/src/__tests__/index.test.ts
+++ packages/cloudflare-ip/src/__tests__/index.test.ts
@@ -25,3 +25,23 @@ test('isCloudflareIp - ipv6 subnet', () => {
   expect(isCloudflareIp('2405:8100::/32')).toBe(true);
   expect(isCloudflareIp('2405:8100::/16')).toBe(false);
 });
+
+test('compatibility with https://github.com/danneu/cloudflare-ip', () => {
+  // non-cloudflare ips should be false
+  expect(isCloudflareIp('66.249.66.1')).toBe(false);
+  expect(isCloudflareIp('1.1.1.1')).toBe(false);
+  // localhost should be false
+  expect(isCloudflareIp('127.0.0.1')).toBe(false);
+  expect(isCloudflareIp('::1')).toBe(false);
+  // cloudflare ips should pass
+  expect(isCloudflareIp('103.21.244.0')).toBe(true);
+  expect(isCloudflareIp('2400:cb00:0000::0000')).toBe(true);
+});
+test('changes from https://github.com/danneu/cloudflare-ip', () => {
+  // garbage should throw an error
+  // N.B. this deviates from the behaviour of https://github.com/danneu/cloudflare-ip
+  expect(() => (isCloudflareIp as any)()).toThrow();
+  expect(() => (isCloudflareIp as any)('')).toThrow();
+  expect(() => (isCloudflareIp as any)(0)).toThrow();
+  expect(() => (isCloudflareIp as any)('chicken')).toThrow();
+});
